c2ef978fa64fbb5957ff2ef1c35a81816b9280ea
Thomas Minshull <thomasminshull@gmail.com>
2016-08-04 14:56:44 -0700

    add animation on open/close icon

--- StretchySnacks/StretchySnacks/ViewController.swift
+++ StretchySnacks/StretchySnacks/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     //MARK: Outlets
     @IBOutlet weak var navBar: UIView!
     @IBOutlet weak var customNavBarHeight: NSLayoutConstraint!
+    @IBOutlet weak var plusButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,16 +21,23 @@ class ViewController: UIViewController {
 
     //MARK: Actions
     @IBAction func plusIconTapped(sender: AnyObject) {
+        var rotation:Double? = nil
         
         if self.customNavBarHeight.constant == 64 {
             self.customNavBarHeight.constant = 200
-        
+            rotation = 40.0
         } else {
             self.customNavBarHeight.constant = 64
+            rotation = 0.0
         }
         
         UIView.animateWithDuration(0.8, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1.0, options:[] , animations: {
-        self.view.layoutIfNeeded()
+            
+            if let rotation = rotation {
+                self.plusButton.transform = CGAffineTransformMakeRotation(CGFloat(rotation))
+            }
+            
+            self.view.layoutIfNeeded()
         }, completion: nil)
 
 
