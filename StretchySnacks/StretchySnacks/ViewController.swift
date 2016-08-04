02717de8b10f13eae26b6ede1ac0dd8f20d75599
Thomas Minshull <thomasminshull@gmail.com>
2016-08-04 14:02:21 -0700

    refactored to use UIView animiation with spring dampening

--- StretchySnacks/StretchySnacks/ViewController.swift
+++ StretchySnacks/StretchySnacks/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var navBar: UIView!
     @IBOutlet weak var customNavBarHeight: NSLayoutConstraint!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +20,19 @@ class ViewController: UIViewController {
 
     //MARK: Actions
     @IBAction func plusIconTapped(sender: AnyObject) {
+        
         if self.customNavBarHeight.constant == 64 {
             self.customNavBarHeight.constant = 200
+        
         } else {
             self.customNavBarHeight.constant = 64
         }
         
-        UIView.animateWithDuration(1.0) {
-            self.navBar.layoutIfNeeded()
-        }
+        UIView.animateWithDuration(0.8, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1.0, options:[] , animations: {
+        self.view.layoutIfNeeded()
+        }, completion: nil)
+
+
     }
 
     
